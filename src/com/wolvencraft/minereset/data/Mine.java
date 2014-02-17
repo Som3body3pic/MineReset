51f04edd63fa4363b514ebd7d558370057347f00
Kirill Bondarchuk <bondarchuk.kirill@gmail.com>
2014-02-17 13:50:04 -0800

    Import file saving and deletion from PrisonMine

--- src/com/wolvencraft/minereset/data/Mine.java
+++ src/com/wolvencraft/minereset/data/Mine.java
@@ -20,10 +20,19 @@
 
 package com.wolvencraft.minereset.data;
 
+import java.io.File;
+import java.io.FileFilter;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.logging.Level;
 
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.configuration.serialization.ConfigurationSerializable;
+
+import com.wolvencraft.minereset.MineReset;
+import com.wolvencraft.unity.util.Message;
 
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -61,6 +70,46 @@ public class Mine implements ConfigurationSerializable {
         map.put("name", name);
         map.put("region", region);
         return map;
+    }
+    
+    /**
+     * Saves the mine data to file.
+     * @return <b>true</b> if the save was successful, <b>false</b> if an error occurred
+     */
+    public boolean saveFile() {
+        File mineFile = new File(new File(MineReset.getInstance().getDataFolder(), "mines"), id + ".mdata.yml");
+        FileConfiguration mineConf =  YamlConfiguration.loadConfiguration(mineFile);
+        mineConf.set("mine", this);
+        try {
+            mineConf.save(mineFile);
+        } catch (IOException e) {
+            Message.log(Level.SEVERE, "Unable to serialize mine '" + id + "'!");
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+     
+    /**
+     * Deletes the mine data file.<br />
+     * <b>Warning:</b> invoking this method will not remove the mine from the list of active mines
+     * @return <b>true</b> if the deletion was successful, <b>false</b> if an error occurred
+     */
+    public boolean deleteFile() {
+        File mineFolder = new File(MineReset.getInstance().getDataFolder(), "mines");
+        if(!mineFolder.exists() || !mineFolder.isDirectory()) return false;
+         
+        File[] mineFiles = mineFolder.listFiles(new FileFilter() {
+            public boolean accept(File file) { return file.getName().contains(".mdata.yml"); }
+        });
+         
+        for(File mineFile : mineFiles) {
+            if(mineFile.getName().equals(id + ".pmine.yml")) {
+                return mineFile.delete();
+            }
+        }
+         
+        return false;
     }
     
 }
